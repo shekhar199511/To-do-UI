02f8cc4a53709778fcb7e37e41385b45a96c9c52
Ravi Shekhar <ravishekhar@Ravis-MacBook-Pro.local>
2023-12-09 13:25:16 +0530

    added profile page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, {Fragment, useState} from 'react';
 
 import Header from './components/UI/Header/Header';
 import Layout from './components/Layout/Layout';
-
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Profile from './pages/Profile/Profile'
 function App() {
 
   return (
-    <div>
-      <Header/>
-      <Layout />
-    </div>
+    <Router>    
+      <div>
+        <Header/>
+        <Switch>
+          <Route path="/profile" component={Profile}/>
+          <Route path="/" component={Layout}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Profile/Profile.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import profile from '../../assets/profile.jpeg';
+import classes from "./Profile.module.css";
+const Profile = (props) => {
+    return (
+        <div className={classes["profile-container"]}>
+
+        <img src={profile} alt="pic of road"/>
+
+        <p>
+            Hi, the image is on our trip to goa. It seems beautiful, doesn't it. 
+            This is the repo in which I have practiced almost all important JS/React/CSS concepts.
+            <br/> <br/>
+            Losing a job opened my eyes. I mean nothing, absolutely nothing is permanent. I was supposed to be the solution, instead I have become poison, the burden.
+
+            In the quest of trying to be everything, I am losing myself. The more I try to become more responsible, the more I making mistakes.
+            <br/> <br/>
+            I could go on.... but I can sum things up on what I have become through one of my favourite quotes : <br/><br/>
+
+            <p className={classes["quote-container"]}><span className={classes.quote}>If you stare into the abyss, the abyss stares back at you.</span> - Nietzsche</p>
+        </p>
+        </div>
+        
+    )
+}
+
+export default Profile
